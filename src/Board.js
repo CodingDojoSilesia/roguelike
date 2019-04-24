1945ff0be6787123f8faf9af3a52a67a1288a275
Michal Klich <michal@michalklich.com>
2019-04-24 23:13:22 +0200

    Add player to the board
    Add player class
    Add support for player commands
    Display player on board

--- src/Board.js
+++ src/Board.js
@@ -6,24 +6,58 @@ const boardWidth = 28;
 const boardHeight = 14;
 
 let board = [];
+let pawns = [];
 
 function init() {
-  for (let i = 0; i < boardHeight; i++) {
-    board[i] = [];
-    for (let j = 0; j < boardWidth; j++) {
-      board[i][j] = floor;
+    for (let i = 0; i < boardHeight; i++) {
+        board[i] = [];
+        for (let j = 0; j < boardWidth; j++) {
+            board[i][j] = floor;
+        }
     }
-  }
 }
 
 function write() {
-  for (let i = 0; i < boardHeight; i++) {
-    console.log(board[i].join(''));
-  }
+    // place pawns
+    pawns.forEach((pawn) => {
+        let [i, j] = pawn.location;
+        board[i][j] = pawn.symbol;
+    });
+
+    for (let i = 0; i < boardHeight; i++) {
+        console.log(board[i].join(''));
+    }
+}
+
+let randomLocation = function fetchRandomLocation() {
+    let location = [
+        Math.floor(Math.random() * boardWidth),
+        Math.floor(Math.random() * boardHeight)
+    ];
+    pawns.forEach((pawn) => {
+        if (pawn.location == location) {
+            return randomLocation();
+        }
+    });
+
+    return location;
+};
+
+let addPawn = function addPawnWithRandomPosition(pawn) {
+    const location = randomLocation();
+    pawns.push(pawn);
+    return location;
+};
+
+let up = function movePawnUp(currentLocation) {
+    console.log(currentLocation)
+
 }
 
 module.exports = {
-  board: board,
-  init: init,
-  write: write
+    board: board,
+    init: init,
+    write: write,
+    addPawn: addPawn,
+    up: up
 };
--- src/index.js
+++ src/index.js
@@ -4,24 +4,63 @@ const readline = require('readline');
 const Board = require('./Board');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: '> '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
 });
 
+
+let Player = class {
+    constructor(board, hp = 20) {
+        this.hp = hp;
+        this.board = board;
+        this.location = undefined;
+        this.symbol = "P";
+    }
+
+    drop() {
+        this.location = this.board.addPawn(this);
+    }
+
+    up() {
+        this.board.up(this.location);
+    }
+};
+
+const player = new Player(Board);
+
 rl.on('line', (line) => {
-  switch (line.trim()) {
-    default:
-      Board.write();
-      console.log(`Your input was '${line.trim()}'`);
-      break;
-  }
-  rl.prompt();
+    switch (line.trim()) {
+        case "up":
+            player.up();
+            break;
+        case "down":
+            break;
+        case "left":
+            break;
+        case "right":
+            break;
+            // TODO: hero actions go here
+            // player will keep his position
+            // when moved he will ask board for his new position
+            // board will keep monster positions, or references to objects and ask
+            // them for their positions, as player keeps it's own so should monsters
+            // BOARD keeps a list of objects, dimensions and is only to
+            // return/calculate new position
+            // if there is monster in hero's way action should be attack instead of move
+        default:
+            Board.write();
+            console.log(`Your input was '${line.trim()}'`);
+            break;
+    }
+    rl.prompt();
 }).on('close', () => {
-  console.log('Have a great day!');
-  process.exit(0);
+    console.log('Have a great day!');
+    process.exit(0);
 });
 
 Board.init();
+// add monsters
+player.drop()
 Board.write();
 rl.prompt();
